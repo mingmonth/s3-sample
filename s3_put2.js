347b2b407d9a3d643b1081af7a46e96678e0b371
yoonsoo <yoonsoo@hknetworks.kr>
2019-11-27 00:15:56 +0900

    s3 풋

--- s3_put2.js
+++ s3_put2.js
@@ -2,13 +2,13 @@ var AWS = require("aws-sdk");
 var fs = require("fs");
 //AWS.config.region = "ap-northeast-2";
 const path = require("path");
-AWS.config.loadFromPath(path.join(__dirname, "config.json"));
+//AWS.config.loadFromPath(path.join(__dirname, "config.json"));
 var s3 = new AWS.S3();
 var param = {
-  Bucket: "skin-test-public",
-  Key: "logo.png",
+  Bucket: "skin-test-public/skintest/upload",
+  Key: "111111.jpg",
   ACL: "public-read",
-  Body: fs.createReadStream("smarthome_logo.png"),
+  Body: fs.createReadStream("111111.jpg"),
   ContentType: "image/png"
 };
 s3.upload(param, function(err, data) {
